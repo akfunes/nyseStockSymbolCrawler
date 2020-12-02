6f1366ab56adece9b683062cd0f34c4b2b00cd14
Austin Funes <austinfunes@gmail.com>
2020-12-01 21:11:35 -0800

    Converted test script to become main run script for the crawler

---
+++ main.py
@@ -0,0 +1,9 @@
+import nyseCrawler
+
+def main():
+
+    crawler = nyseCrawler.nyseCrawler()
+    crawler.getAllPages()
+
+if __name__ == "__main__":
+    main()
--- testNyse.py
+++
@@ -1,6 +0,0 @@
-import nyseCrawler
-
-crawler = nyseCrawler.nyseCrawler()
-print(crawler.getAllPages())
-#if crawler.getSinglePage(675).text == '[]':
-#    print(True)
